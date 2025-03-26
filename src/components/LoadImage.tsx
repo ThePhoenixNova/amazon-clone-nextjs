061d17b1236417ff3c365e5b4606b0dae5adb346
Phoenix Nova <thephoenixnova@gmail.com>
2025-03-26 14:05:20 +0700

    Version 12 updated

--- src/components/LoadImage.tsx
+++ src/components/LoadImage.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from "react";
 import Image from "next/image";
-
+//import something here for interactivity
 interface LoadAndZoomImageProps {
   imgSrc: string;
   zoomLevel?: number;
